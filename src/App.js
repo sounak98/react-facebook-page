15de704f55b9f7cbb5162724ede9f85d0ba18bd2
Sounak Pradhan <sounak.98@gmail.com>
2019-10-11 19:43:46 +0530

    Update

--- src/App.js
+++ src/App.js
@@ -1,45 +1,82 @@
 import React, { Component } from "react";
 import "./App.css";
 
-import FacebookLoginButton from "./components/FacebookLogin";
+import graph from "fbgraph";
+import FacebookLogin from "react-facebook-login";
 
 class App extends Component {
   state = {
-    username: null
+    name: null,
+    accessToken: null,
+    comments: null
   };
 
-  // componentDidMount() {
-  //   window.FB.getLoginStatus(response => {
-  //     console.log(response);
-  //   })
-  // }
-
-  onFacebookLogin = (loginStatus, resultObject) => {
-    console.log(loginStatus, resultObject);
-    if (loginStatus === true) {
-      this.setState({
-        username: resultObject.user.name
-      });
+  onFacebookLogin = response => {
+    if (!response.error) {
+      const { name, accessToken } = response;
+      this.setState({ name, accessToken });
     } else {
-      alert("Login err");
+      console.error(response.error);
     }
   };
 
+  fetchComments = id => {
+    graph.get(`${id}/posts/?fields=comments,message`, (err, response) => {
+      if (!err) {
+        this.setState({ comments: response.data }, () => {
+          console.log(this.state.comments);
+        });
+      } else {
+        console.error(err);
+      }
+    });
+  };
+
+  fetchPosts = () => {
+    graph.setAccessToken(this.state.accessToken);
+    graph.get("me/accounts", (err, response) => {
+      if (!err) {
+        const { name, id } = response.data[0];
+        console.log(name, id);
+        this.fetchComments(id);
+      } else {
+        console.error(err);
+      }
+    });
+  };
+
   render() {
-    const { username } = this.state;
     return (
-      <div className="App">
-        <div className="App-intro">
-          {!username && (
-            <div>
-              <p>Click on one of any button below to login</p>
-              <FacebookLoginButton onLogin={this.onFacebookLogin}>
-                <button>Facebook</button>
-              </FacebookLoginButton>
-            </div>
-          )}
-          {username && <p>Welcome back, {username}</p>}
-        </div>
+      <div>
+        {this.state.accessToken ? (
+          <div>
+            <p>Name: {this.state.name}</p>
+            <p>Access Token: {this.state.accessToken}</p>
+            <button onClick={this.fetchPosts}>Fetch Posts</button>
+            {this.state.comments && (
+              <ul>
+                {this.state.comments.map((post, idx) => (
+                  <li key={idx}>
+                    {post.message}
+                    <ol>
+                      {post.comments.data.map((comment, idx) => (
+                        <li key={idx}>{comment.message}</li>
+                      ))}
+                    </ol>
+                  </li>
+                ))}
+              </ul>
+            )}
+          </div>
+        ) : (
+          <FacebookLogin
+            appId="389126295360521"
+            autoLoad={true}
+            fields="name"
+            scope="manage_pages"
+            callback={this.onFacebookLogin}
+          />
+        )}
       </div>
     );
   }

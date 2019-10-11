a4315d2556faf9c7b38ee9aa8bc853fa26b07f4b
Sounak Pradhan <sounak.98@gmail.com>
2019-10-11 09:13:23 +0530

    Add base code for React Facebook Login Component

--- src/App.js
+++ src/App.js
@@ -1,26 +1,48 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import FacebookLoginButton from "./components/FacebookLogin";
+
+class App extends Component {
+  state = {
+    username: null
+  };
+
+  // componentDidMount() {
+  //   window.FB.getLoginStatus(response => {
+  //     console.log(response);
+  //   })
+  // }
+
+  onFacebookLogin = (loginStatus, resultObject) => {
+    console.log(loginStatus, resultObject);
+    if (loginStatus === true) {
+      this.setState({
+        username: resultObject.user.name
+      });
+    } else {
+      alert("Login err");
+    }
+  };
+
+  render() {
+    const { username } = this.state;
+    return (
+      <div className="App">
+        <div className="App-intro">
+          {!username && (
+            <div>
+              <p>Click on one of any button below to login</p>
+              <FacebookLoginButton onLogin={this.onFacebookLogin}>
+                <button>Facebook</button>
+              </FacebookLoginButton>
+            </div>
+          )}
+          {username && <p>Welcome back, {username}</p>}
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
